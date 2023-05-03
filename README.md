2076e35d082dc78afa9eef94456158c7ee62904d
user <eylonnaamat@gmail.com>
2023-05-03 12:20:26 +0300

    working access point & few readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Evil_Twin_Attack_Tool
+
+installations:
+sudo apt install net-tools </br>
+sudo apt-get install hostapd isc-dhcp-server </br>
+
--- ap_setup.py
+++ ap_setup.py
@@ -7,8 +7,11 @@ def set_adapter_to_monitor(iface_wifi):
     os.system(f"sudo iwconfig {iface_wifi} mode monitor")
     os.system(f"sudo ifconfig {iface_wifi} up")
 
+def set_default_gateway(iface_wifi, gateway_ip):
+    os.system(f"sudo route add default gw {gateway_ip} {iface_wifi}")
+    subprocess.run(['sysctl', '-w', 'net.ipv4.ip_forward=1'], capture_output=True, text=True)
 
-def start_ap(iface_wifi):
+def start_ap(iface_wifi, iface_router, gateway_ip):
     # Stop any existing DHCP servers
     subprocess.call(['sudo', 'service', 'isc-dhcp-server', 'stop'])
 
@@ -29,14 +32,25 @@ def start_ap(iface_wifi):
     subprocess.call(['sudo', 'hostapd', '-B', '/etc/hostapd/hostapd.conf'])
     subprocess.call(['sudo', 'service', 'isc-dhcp-server', 'start'])
 
+    # Set the default gateway to be the router's IP address
+    set_default_gateway(iface_wifi, gateway_ip)
+
+    # Enable IP forwarding on the router interface
+    subprocess.run(['sudo', 'sysctl', '-w', 'net.ipv4.ip_forward=1'], capture_output=True, text=True)
+    subprocess.call(['sudo', 'iptables', '-t', 'nat', '-A', 'POSTROUTING', '-o', iface_router, '-j', 'MASQUERADE'])
+
     # Wait for the access point to start
     time.sleep(5)
 
-def stop_ap(iface):
+def stop_ap(iface_wifi, iface_router):
+    # Disable IP forwarding on the router interface
+    subprocess.call(['sudo', 'iptables', '-t', 'nat', '-D', 'POSTROUTING', '-o', iface_router, '-j', 'MASQUERADE'])
+    subprocess.run(['sudo', 'sysctl', '-w', 'net.ipv4.ip_forward=0'], capture_output=True, text=True)
+
     # Stop the access point
     subprocess.call(['sudo', 'service', 'isc-dhcp-server', 'stop'])
-    subprocess.call(['sudo', 'hostapd', '-B', '/etc/hostapd/hostapd.conf', '-i', iface, '-K'])
-    subprocess.call(['sudo', 'ifconfig', iface, 'down'])
+    subprocess.call(['sudo', 'hostapd', '-B', '/etc/hostapd/hostapd.conf', '-i', iface_wifi, '-K'])
+    subprocess.call(['sudo', 'ifconfig', iface_wifi, 'down'])
 
 def create_ap_config(ssid, password, iface_wifi):
     config_file = f"""interface={iface_wifi}
@@ -51,17 +65,13 @@ channel=6"""
     with open("/etc/hostapd/hostapd.conf", "w") as f:
         f.write(config_file)
 
-
-
-
 interface = "wlxc83a35c2e0a2"
 ssid = "eylon&michael"
 password = "E1y2!3o4n5"
+gateway_ip = "192.168.1.1"  # IP address of the router
+iface_router = "wlp0s20f3"
 
 set_adapter_to_monitor(interface)
-start_ap(interface)
+start_ap(interface, iface_router, gateway_ip)
 create_ap_config(ssid, password, interface)
 # stop_ap(interface)
-
-
-
